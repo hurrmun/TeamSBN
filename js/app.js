41d6faba89c4fc5fa19d122bd222be555d7e4ae2
hermanlyx <hermanlyx@gmail.com>
2021-09-19 12:01:41 +0800

    added add new member button and generate mock list button function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 //* Write functions here:
-console.log($)
 
+const $createNewFamilyMember = () => {
+    const $familyMember1 = $(".familyMember1")
+    // console.log($familyMember1)
+    const $familyMembers = $(".familyMembers")
+    $familyMember1.clone(true).removeClass("familyMember1").appendTo($familyMembers)
+}
 
-
+const $showGroceryList = () => {
+    $(".groceryList").show()
+}
 
 
 
@@ -11,7 +18,13 @@ console.log($)
 //* Run functions here:
 
 const main = () => {
-
+    const $newFamily = $(".addNewFamily") 
+    $newFamily.on("click", $createNewFamilyMember)
+    const $groceryList = $(".groceryList")
+    $groceryList.hide()
+    const $groceryButton = $(".groceryListButton")
+    $groceryButton.on("click", $showGroceryList)
 }
 
 $(main)
+
